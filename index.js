473af3b89dd08ebc7bc0ce85c6a75de16cbd2d04
Robert Shepley <shepleysound@gmail.com>
2022-09-26 17:19:00 -0700

    Move initialization logic to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { sequelizeDB } = require('./src/models');
-const { app } = require('./src/server');
+// const { sequelizeDB } = require('./src/models');
+const { app, sequelizeDB } = require('./src/app');
 const PORT = process.env.PORT || 3002;
 require('dotenv').config();
 
--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@
 // 3rd Party Resources
 const express = require('express');
 const bcrypt = require('bcrypt');
-const base64 = require('base-64');
 const { Sequelize, DataTypes } = require('sequelize');
 
 // NOTE: connected to sqlite::memory out of box for proof of life
@@ -19,13 +18,13 @@ const app = express();
 // Process JSON input and put the data on req.body
 app.use(express.json());
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelizeDB = new Sequelize(DATABASE_URL);
 
 // Process FORM intput and put the data on req.body
 app.use(express.urlencoded({ extended: true }));
 
 // Create a Sequelize model
-const Users = sequelize.define('User', {
+const Users = sequelizeDB.define('User', {
   username: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -33,7 +32,7 @@ const Users = sequelize.define('User', {
   password: {
     type: DataTypes.STRING,
     allowNull: false,
-  }
+  },
 });
 
 // Signup Route -- create a new user
@@ -91,12 +90,4 @@ app.post('/signin', async (req, res) => {
 
 });
 
-// make sure our tables are created, start up the HTTP server.
-sequelize.sync()
-  .then(() => {
-    app.listen(3000, () => console.log('server up'));
-  }).catch(e => {
-    console.error('Could not start server', e.message);
-  });
-
-module.exports = { app, sequelize }
+module.exports = { app, sequelizeDB };
